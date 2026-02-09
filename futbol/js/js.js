deecf469c8a66841cb0734a5c8cd6f25b2c89c7f
JP-666 <jcargue0501@g.educaand.es>
2026-02-09 13:15:53 +0100

    Pruebas de smooth scrolling en varios dispositivos y arreglo final para moviles

--- futbol/js/js.js
+++ futbol/js/js.js
@@ -10,8 +10,13 @@ function MoverAIDY(id_elemento) { /* Nos mueve al elemento (Posicion Y) cuando l
 		elemento.scrollIntoView({
 			behavior: 'smooth',
 			block: 'start'
+			/* Al parecer en WebKit esto funciona diferente, tiene como un pequeño retraso antes de iniciar */
+			/* Segun internet, esto se debe a que el motor de renderizado de WebKit almacena los efectos de */
+			/* Scrolling en un buffer, y hace un 'flush' al 'x' tiempo (No se sabe, aunque estimo que es    */
+			/* Unos 800-900 Milisegundos. La solucion mas elegante que he visto es meter toda la funcion en */
+			/* Un setTimeout(), pero esto queda feo. En fin, cosas de Apple               ¯\_(ツ)_/¯        */
 		});
 	} else {
-		console.warn("OOPS! No encontrado; " + idElemento);
+		console.warn("OOPS! No encontrado; " + id_elemento); /* Esto estaba roto. Pero como mi codigo es obviamente perfecto nunca me he topado con este elemento */
 	}
 } 
