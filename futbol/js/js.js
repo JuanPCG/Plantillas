9eec97ebc0e1dac5dd6f6134802559b230d90c61
JP-666 <jcargue0501@g.educaand.es>
2026-02-17 09:53:57 +0100

    Arreglado safari

--- futbol/js/js.js
+++ futbol/js/js.js
@@ -1,22 +1,36 @@
+let EnSafari=true
 function AbrirEnlace(enlace) {
 	window.open(enlace, '_blank').focus();
 }
 
-function MoverAIDY(id_elemento) { /* Nos mueve al elemento (Posicion Y) cuando lo llamamos */
-		/* Mover a ID Y */
+function MoverAIDY(id_elemento) {
 	const elemento = document.getElementById(id_elemento);
-	
 	if (elemento) {
-		elemento.scrollIntoView({
-			behavior: 'smooth',
-			block: 'start'
-			/* Al parecer en WebKit esto funciona diferente, tiene como un pequeño retraso antes de iniciar */
-			/* Segun internet, esto se debe a que el motor de renderizado de WebKit almacena los efectos de */
-			/* Scrolling en un buffer, y hace un 'flush' al 'x' tiempo (No se sabe, aunque estimo que es    */
-			/* Unos 800-900 Milisegundos. La solucion mas elegante que he visto es meter toda la funcion en */
-			/* Un setTimeout(), pero esto queda feo. En fin, cosas de Apple               ¯\_(ツ)_/¯        */
-		});
+		if (EnSafari) {
+			PosicionE = elemento.getBoundingClientRect();
+			window.scrollTo({
+				"top": PosicionE.top,
+				"behavior": "smooth"
+			});
+		}
+		else {
+			elemento.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		}
 	} else {
 		console.warn("OOPS! No encontrado; " + id_elemento); /* Esto estaba roto. Pero como mi codigo es obviamente perfecto nunca me he topado con este elemento */
 	}
-} 
+}
+
+function detectar_safari() {
+	if (! navigator.safari) {
+		/* console.log("No estamos en safari") */
+		EnSafari = false
+	} else {
+		/* console.log("Estamos en safari") */
+		/* document.getElementById("Cambiame").innerHTML="Hola, estas usando Safari" */
+		EnSafari = true
+	}
+}
